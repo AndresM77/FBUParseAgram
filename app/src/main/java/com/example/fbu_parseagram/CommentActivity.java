aaaad0ad32e7a7545f048b3dc6d65b650b663444
Andres Montoya <andresmontoya7@fb.com>
2019-07-11 17:29:54 -0700

    Implemented Comment functionality

--- app/src/main/java/com/example/fbu_parseagram/CommentActivity.java
+++ app/src/main/java/com/example/fbu_parseagram/CommentActivity.java
@@ -1,8 +1,10 @@
 package com.example.fbu_parseagram;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,20 +17,47 @@ import com.parse.SaveCallback;
 public class CommentActivity extends AppCompatActivity {
 
     public final static String TAG = "CommentActivity";
+    public final static int RESULT_CODE = 20;
     public Post post;
+    private Button btnComment;
+    private Button btnReturn;
+    private EditText etComment;
+    private ParseUser user;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
+
+        //Initialize view variables
+        btnComment = findViewById(R.id.btnComment);
+        etComment = findViewById(R.id.etComment);
+        btnReturn = findViewById(R.id.btnReturn);
+
         post = getIntent().getParcelableExtra("post");
 
+        user = ParseUser.getCurrentUser();
+
+        btnComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                comment(post, user, etComment.getText().toString());
+            }
+        });
+
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
-    public void comment(final Post post) {
+    public void comment(final Post post, ParseUser user, String text) {
         ParseComment comment = new ParseComment();
-        comment.setUser(ParseUser.getCurrentUser());
+        comment.setUser(user);
         comment.setPost(post);
+        comment.setParseComment(text);
         comment.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
@@ -38,6 +67,7 @@ public class CommentActivity extends AppCompatActivity {
                     return;
                 }
                 Log.d(TAG, "Success, comment saved");
+                finish();
             }
         });
     }
